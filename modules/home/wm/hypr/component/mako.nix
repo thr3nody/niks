bdcc6b7a0528a3572bb5bedad8a0797241f4548c
Erine Moira <me@erine.dev>
2024-10-23 22:52:27 +0700

    Mmm... Notification is good now...

--- modules/home/wm/hypr/component/mako.nix
+++ modules/home/wm/hypr/component/mako.nix
@@ -3,11 +3,14 @@
 {
   services.mako = with config.colorScheme.palette; {
     enable = true;
+    actions = true;
     backgroundColor = "#${base01}";
-    borderColor = "#${base06}";
+    borderColor = "#${base05}";
     borderRadius = 5;
     borderSize = 2;
+    padding = "10,20,10,20";
     textColor = "#${base06}";
     layer = "overlay";
+    defaultTimeout = 5000;
   };
 }
