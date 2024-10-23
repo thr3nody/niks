1d59b46254a3b044e2c4a8f35e1201b608d51687
Erine Moira <me@erine.dev>
2024-10-23 22:52:04 +0700

    Mmm.. Wofi...

--- modules/home/wm/hypr/component/wofi.nix
+++ modules/home/wm/hypr/component/wofi.nix
@@ -1,7 +1,27 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.wofi = {
     enable = true;
+    style = let
+      inherit (config.colorScheme) palette;
+    in
+    ''
+      * {
+        border-radius: 10px;
+      }
+
+      #window {
+        background-color: #${palette.base02};
+        padding: 1em;
+      }
+
+      #input {
+        backgound-color: #${palette.base03}:
+        color: #${palette.base07}:
+        margin-bottom: 0.5em;
+        border-color: #${palette.base09};
+      }
+    '';
   };
 }
