54de48bb08071dc2e46c90fe5cf8eb6a107f947f
Erine Moira <me@erine.dev>
2024-11-01 09:54:15 +0700

    Forgor to import gnome

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -18,6 +18,7 @@
     ../../modules/system/networking/firewall.nix
     ../../modules/system/networking/tailscale.nix
     ../../modules/system/themes/fonts.nix
+    ../../modules/system/de/gnome.nix
   ];
 
   # Bootloader.
