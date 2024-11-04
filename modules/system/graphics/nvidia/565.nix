95139a894629b797247e921b0a1b9802f18c09db
Erine Moira <me@erine.dev>
2024-11-04 10:15:56 +0700

    Extra modules stolen from github.

--- modules/system/graphics/nvidia/565.nix
+++ modules/system/graphics/nvidia/565.nix
@@ -2,7 +2,8 @@
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
-    nvidiaSettings = false;
+    powerManagement.enable = true; # Needed for suspend to behave correctly.
+    nvidiaSettings = false; # Disable until 0384602eac8bc57add3227688ec242667df3ffe3the hits me in the balls
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
       version = "565.57.01";
       sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
@@ -22,5 +23,13 @@
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
+  };
+
+  environment.variables = { # Stolen from https://github.com/TLATER/dotfiles/blob/f4ed53e9e78ff41cd2849f389499d3bfd3ffe4c7/nixos-config/hosts/yui/nvidia/default.nix
+    # Required to run the correct GBM backend for nvidia GPUs on wayland
+    GBM_BACKEND = "nvidia-drm";
+    # Apparently, without this nouveau may attempt to be used instead
+    # (despite it being blacklisted)
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
   };
 }
