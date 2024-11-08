1e7cc349a3b8214c52d1df2e968ecef38f8d2d09
Erine Moira <me@erine.dev>
2024-11-08 08:57:01 +0700

    Flakey lock.

--- modules/home/coding/nvim/theme.nix
+++
@@ -1,7 +0,0 @@
-_: {
-  programs.nvf.settings.vim.theme = {
-    enable = true;
-    name = "gruvbox";
-    style = "dark";
-  };
-}
--- modules/home/coding/nvim/visuals.nix
+++
@@ -1,8 +0,0 @@
-_: {
-  programs.nvf.settings.vim.visuals = {
-    highlight-undo.enable = true;
-    indentBlankline.enable = true;
-    nvimWebDevicons.enable = true;
-    smoothScroll.enable = true;
-  };
-}
