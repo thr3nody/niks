9039e75c2c16e592c957af9dc6e2d6a8ca0710a2
Erine Moira <me@erine.dev>
2024-12-12 16:27:56 +0700

    Placed the wrong thing in the wrong spot(?)

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -220,9 +220,10 @@
       gamescopeSession.enable = true;
     };
     gamemode.enable = true;
-    environment.sessionVariables = {
-      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-    };
+  };
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
   };
 
   # Virt Manager
