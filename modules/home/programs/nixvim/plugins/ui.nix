598d5779ebaf2d39bbf796f3dc30efc0eaf23525
Erine Moira <me@erine.dev>
2025-01-21 22:00:40 +0700

    Nixvim Notify background colour thing.

--- modules/home/programs/nixvim/plugins/ui.nix
+++ modules/home/programs/nixvim/plugins/ui.nix
@@ -5,6 +5,7 @@
     notify = {
       enable = true;
       level = "warn";
+      backgroundColour = "#32302F";
     };
     gitsigns = {
       enable = true;
@@ -22,8 +23,8 @@
         ];
         alwaysDivideMiddle = true;
         globalstatus = true;
-        ignoreFocus = [ "neo-tree" ];
-        extensions = [ "fzf" ];
+        ignoreFocus = ["neo-tree"];
+        extensions = ["fzf"];
         theme = "auto";
         componentSeparators = {
           left = "|";
@@ -34,12 +35,12 @@
           right = "█"; # 
         };
         sections = {
-          lualine_a = [ "mode" ];
-          lualine_b = [ "branch" "diff" "diagnostics" ];
-          lualine_c = [ "filename" ];
-          lualine_x = [ "filetype" ];
-          lualine_y = [ "progress" ];
-          lualine_z = [ ''" " .. os.date("%R")'' ];
+          lualine_a = ["mode"];
+          lualine_b = ["branch" "diff" "diagnostics"];
+          lualine_c = ["filename"];
+          lualine_x = ["filetype"];
+          lualine_y = ["progress"];
+          lualine_z = [''" " .. os.date("%R")''];
         };
       };
     };
