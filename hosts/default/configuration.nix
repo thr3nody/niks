14dca23212aa24942d7665380f9acbe70d3d609c
Erine Moira <me@erine.dev>
2025-02-02 01:52:07 +0700

    Modularity for bash.

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -171,6 +171,7 @@
     # Coding Stuff
     lunarvim
     vscode
+    thonny
 
     php
     php82Packages.composer
@@ -228,17 +229,6 @@
     extensions = [
       "cjpalhdlnbpafiamejdnhcphjbkeiagm"
     ];
-  };
-
-  # Bash
-  programs.bash = {
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
   };
 
   # Polkit
