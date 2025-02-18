b5c3b0db8f5406ab0a245e5f633c7bb59ddbcd5f
Erine Moira <me@erine.dev>
2025-02-18 08:31:04 +0700

    Fix kitty keybindings.

--- modules/home/programs/kitty/keybindings.nix
+++ modules/home/programs/kitty/keybindings.nix
@@ -1,7 +1,9 @@
 {
   programs.kitty.keybindings = {
     "ctrl+c" = "copy_or_interrupt";
-    "ctrl+shift+>" = "next_tab";
-    "ctrl+shift+<" = "previous_tab";
+    "ctrl+shift+." = "next_tab";
+    "ctrl+shift+," = "previous_tab";
+    "ctrl+shift+right" = "move_tab_forward";
+    "ctrl+shift+left" = "move_tab_forward";
   };
 }
---
+++ modules/home/programs/kitty/settings.nix
@@ -0,0 +1,16 @@
+{
+  programs.kitty.settings = {
+    confirm_os_window_close = false;
+    hide_window_decorations = true;
+
+    tab_bar_align = "center";
+
+    detect_urls = true;
+    url_style = "curly";
+
+    mouse_hide_wait = 3.0;
+    cursor_shape = "block";
+    cursor_shape_unfocused = "underline";
+    scrollback_lines = 3000;
+  };
+}
