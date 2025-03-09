cfdccc69a1e2b28e0b1cb664ceb4b4ddceb1541c
Erine Moira <me@erine.dev>
2025-03-09 17:17:29 +0700

    Remove color helper from modules.

--- modules/home/wm/components/waybar.nix
+++ modules/home/wm/components/waybar.nix
@@ -107,25 +107,6 @@
       };
     };
 
-    # Styling Look like this, goes from darker color (top) to lighter color (bottom).
-    # Name    Value                 Example with #${palette.baseXX}
-    # base00: "282828" # ----       #282828
-    # base01: "3c3836" # ---        #3c3836
-    # base02: "504945" # --         #504945
-    # base03: "665c54" # -          #665c54
-    # base04: "bdae93" # +          #bdae93
-    # base05: "d5c4a1" # ++         #d5c4a1
-    # base06: "ebdbb2" # +++        #ebdbb2
-    # base07: "fbf1c7" # ++++       #fbf1c7
-    # base08: "fb4934" # red        #fb4934
-    # base09: "fe8019" # orange     #fe8019
-    # base0A: "fabd2f" # yellow     #fabd2f
-    # base0B: "b8bb26" # green      #b8bb26
-    # base0C: "8ec07c" # aqua/cyan  #8ec07c
-    # base0D: "83a598" # blue       #83a598
-    # base0E: "d3869b" # purple     #d3869b
-    # base0F: "d65d0e" # brown      #d65d0e
-
     style = let
       inherit (config.colorScheme) palette;
     in "
