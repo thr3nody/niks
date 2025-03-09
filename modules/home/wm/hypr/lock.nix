d44a6abc41972f169eaaa866ab5edcd51fd547f5
Erine Moira <me@erine.dev>
2025-03-09 17:17:45 +0700

    Hyprlock now good sheeeshhhh!

--- modules/home/wm/hypr/lock.nix
+++ modules/home/wm/hypr/lock.nix
@@ -13,29 +13,9 @@
 
       background = [
         {
-          path = "$HOME/Pictures/taipestory.jpeg";
+          path = "$HOME/Pictures/Walp/wallhaven-m3vgvk.jpg";
           brightness = 0.7;
           contrast = 0.8;
-        }
-      ];
-
-      input-field = [
-        {
-          size = "256, 64";
-          position = "0, -200";
-          monitor = "";
-          dots_center = true;
-          fade_on_empty = true;
-          font_color = "rgba(${base0A}ee)";
-          inner_color = "rgba(${base0E}ee)";
-          outer_color = "rgba(${base0A}ee)";
-          outline_thickness = 2;
-          shadow_passes = 2;
-          shadow_size = 2;
-          shadow_color = "rgba(${base09}ee)";
-          placeholder_text = "<i>Input Password...</i>";
-          check_color = "rgba(${base06}ee)";
-          fail_color = "rgba(${base08}ee)";
         }
       ];
 
@@ -45,7 +25,8 @@
           text = "cmd[update:1000] date +'%I:%M'";
           color = "rgba(${base0A}ee)";
           font_size = 96;
-          position = "0, 300";
+          font_family = "Fira Code Extrabold";
+          position = "0, 200";
           halign = "center";
           valign = "center";
           shadow_passes = 2;
@@ -57,12 +38,37 @@
           text = "cmd[update:1000] date +'%A, %B %d %Y'";
           color = "rgba(${base0A}ee)";
           font_size = 22;
-          position = "0, 200";
+          font_family = "Fira Code Regular";
+          position = "0, 100";
           halign = "center";
           valign = "center";
           shadow_passes = 2;
           shadow_size = 2;
           shadow_color = "rgba(${base09}ee)";
+        }
+      ];
+
+      input-field = [
+        {
+          size = "256, 48";
+          position = "0, -200";
+          monitor = "";
+          dots_center = true;
+          dots_size = 0.3;
+          dots_spacing = 0.15;
+          dots_text_format = "😘";
+          fade_on_empty = true;
+          font_color = "rgba(${base0A}ee)";
+          inner_color = "rgba(${base03}ee)";
+          outer_color = "rgba(${base0A}ee)";
+          outline_thickness = 2;
+          shadow_passes = 2;
+          shadow_size = 2;
+          shadow_color = "rgba(${base09}ee)";
+          placeholder_text = "<i>Input Password :3</i>";
+          fail_text = "<i>You failed OwO</i>";
+          check_color = "rgba(${base06}ee)";
+          fail_color = "rgba(${base08}ee)";
         }
       ];
     };
