630ea278619918eb27e1b4617b6aebc9f0e0d47f
Erine Moira <me@erine.dev>
2025-05-26 08:39:28 +0700

    Extra stuff for thunar.

--- modules/system/programs/thunar.nix
+++ modules/system/programs/thunar.nix
@@ -8,4 +8,10 @@
       thunar-media-tags-plugin
     ];
   };
+
+  # If xfce is not used as desktop and therefore xfconf is not enabled, preference changes are discarded. In that case enable the xfconf program manually to be able to save preferences.
+  programs.xfconf.enable = true;
+
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
 }
