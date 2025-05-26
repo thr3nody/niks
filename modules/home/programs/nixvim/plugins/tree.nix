88d7d18f70de925d57937603401782821a1b2fad
Erine Moira <me@erine.dev>
2025-05-27 02:36:03 +0700

    Specify neo-tree keymaps explicitly.

--- modules/home/programs/nixvim/plugins/tree.nix
+++ modules/home/programs/nixvim/plugins/tree.nix
@@ -5,6 +5,56 @@
       enableGitStatus = true;
       enableModifiedMarkers = true;
       enableRefreshOnWrite = true;
+      window.mappings = {
+        "<space>" = {
+          command = "toggle_node";
+          # disable `nowait` if you have existing combos starting with this char that you want to use
+          nowait = false;
+        };
+        "<2-LeftMouse>" = "open";
+        "<cr>" = "open";
+        "<esc>" = "revert_preview";
+        P = {
+          command = "toggle_preview";
+          config = {use_float = true;};
+        };
+        l = "open";
+        S = "open_split";
+        # S = "split_with_window_picker";
+        s = "open_vsplit";
+        # s = "vsplit_with_window_picker";
+        t = "open_tabnew";
+        # "<cr>" = "open_drop";
+        # t = "open_tab_drop";
+        w = "open_with_window_picker";
+        C = "close_node";
+        h = "close_node";
+        "." = "toggle_hidden";
+        # H = "set_root";
+        z = "close_all_nodes";
+        # Z = "expand_all_nodes";
+        R = "refresh";
+        a = {
+          command = "add";
+          # some commands may take optional config options, see `:h neo-tree-mappings` for details
+          config = {
+            show_path = "none"; # "none", "relative", "absolute"
+          };
+        };
+        A = "add_directory";
+        d = "delete";
+        r = "rename";
+        y = "copy_to_clipboard";
+        x = "cut_to_clipboard";
+        p = "paste_from_clipboard";
+        c = "copy";
+        m = "move";
+        e = "toggle_auto_expand_width";
+        q = "close_window";
+        "?" = "show_help";
+        "<" = "prev_source";
+        ">" = "next_source";
+      };
     };
     oil = {
       enable = true;
