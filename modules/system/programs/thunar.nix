945ae18e859d303bc329987a80f7ee02245e1ced
Erine Moira <me@erine.dev>
2025-06-04 17:52:17 +0700

    Feat(thunar): Add file-roller archive manager, and do a bit of refactoring.

--- modules/system/programs/thunar.nix
+++ modules/system/programs/thunar.nix
@@ -1,17 +1,24 @@
 {pkgs, ...}: {
-  programs.thunar = {
-    enable = true;
-    plugins = with pkgs.xfce; [
-      thunar-archive-plugin
-      thunar-volman
-      thunar-vcs-plugin
-      thunar-media-tags-plugin
-    ];
+  programs = {
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+        thunar-vcs-plugin
+        thunar-media-tags-plugin
+      ];
+    };
+
+    # If xfce is not used as desktop and therefore xfconf is not enabled, preference changes are discarded. In that case enable the xfconf program manually to be able to save preferences.
+    xfconf.enable = true;
+
+    # Archive manager
+    file-roller.enable = true;
   };
 
-  # If xfce is not used as desktop and therefore xfconf is not enabled, preference changes are discarded. In that case enable the xfconf program manually to be able to save preferences.
-  programs.xfconf.enable = true;
-
-  services.gvfs.enable = true; # Mount, trash, and other functionalities
-  services.tumbler.enable = true; # Thumbnail support for images
+  services = {
+    gvfs.enable = true; # Mount, trash, and other functionalities
+    tumbler.enable = true; # Thumbnail support for images
+  };
 }
