2d6851de01f4e54ec342b13245c483a848efc2ce
Erine Moira <me@erine.dev>
2025-06-05 11:34:06 +0700

    Feat(spicetify): Delete fullAppDisplayMod, add hidePodcasts.

--- modules/home/themes/spicetify.nix
+++ modules/home/themes/spicetify.nix
@@ -19,20 +19,7 @@
     enabledExtensions = with spicePkgs.extensions; [
       keyboardShortcut
       copyLyrics
-      {
-        src =
-          (pkgs.fetchFromGitHub {
-            owner = "huhridge";
-            repo = "huh-spicetify-extensions";
-            rev = "d58201e27716df86ace457a82dce980175c2d65d";
-            hash = "sha256-ZJJC7mGsM6H8rEfX82CF0kkv/2MYHehN+nS7FktV/t4=";
-          })
-          + /fullAppDisplayModified;
-
-        name = "fullAppDisplayMod.js";
-      }
+      hidePodcasts
     ];
-
-    # Give up finding a good theme.
   };
 }
