62c47b0546b8479163cdf85a6abe40cd953d5ec8
Erine Moira <me@erine.dev>
2025-08-04 01:25:22 +0700

    Add: Env for nvidia stuff.

--- modules/system/graphics/nvidia/stable.nix
+++ modules/system/graphics/nvidia/stable.nix
@@ -17,4 +17,11 @@
       nvidiaBusId = "PCI:1:0:0";
     };
   };
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  };
 }
