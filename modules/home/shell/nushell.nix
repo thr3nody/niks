27513c3c70a1dbbfcde7eeb19055d34077826070
Erine Moira <me@erine.dev>
2025-08-07 01:53:01 +0700

    Feat(nushell): Flag change due to deprecation.

--- modules/home/shell/nushell.nix
+++ modules/home/shell/nushell.nix
@@ -73,7 +73,7 @@
         ## alias fixer start https://www.nushell.sh/cookbook/external_completers.html#alias-completions
         let expanded_alias = scope aliases
         | where name == $spans.0
-        | get -i 0.expansion
+        | get -o 0.expansion
 
         let spans = if $expanded_alias != null {
           $spans
