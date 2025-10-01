4e7fff9d180d717adfdc4546239fd33e67075343
Erine Moira <me@erine.dev>
2025-10-01 23:08:41 +0700

    Feat(cursor): Change to a banana cursor because why not.

--- modules/home/themes/cursor.nix
+++ modules/home/themes/cursor.nix
@@ -2,10 +2,16 @@
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
+
     # package = pkgs.bibata-cursors;
-    package = pkgs.pokemon-cursor;
     # name = "Bibata-Modern-Classic";
-    name = "Pokemon";
+
+    # package = pkgs.pokemon-cursor;
+    # name = "Pokemon";
+
+    package = pkgs.banana-cursor;
+    name = "Banana";
+
     size = 24;
   };
 }
