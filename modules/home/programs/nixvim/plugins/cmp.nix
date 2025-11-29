34d679b65e4f3c976b367b5c3a22abbb5c478c49
Erine Moira <me@erine.dev>
2025-11-29 16:08:55 +0700

    Feat(nixvim:cmp): Add dynamic width for cmp.

--- modules/home/programs/nixvim/plugins/cmp.nix
+++ modules/home/programs/nixvim/plugins/cmp.nix
@@ -7,7 +7,6 @@
     cmp-path.enable = true;
     cmp-dap.enable = true;
     cmp_luasnip.enable = true;
-    # blink-cmp.enable = true;
 
     cmp = {
       enable = true;
@@ -53,52 +52,84 @@
         formatting = {
           fields = ["abbr" "kind" "menu"];
           format = ''
-            function(_, item)
-            local icons = {
-              Namespace = "󰌗",
-              Text = "󰉿",
-              Method = "󰆧",
-              Function = "󰆧",
-              Constructor = "",
-              Field = "󰜢",
-              Variable = "󰀫",
-              Class = "󰠱",
-              Interface = "",
-              Module = "",
-              Property = "󰜢",
-              Unit = "󰑭",
-              Value = "󰎠",
-              Enum = "",
-              Keyword = "󰌋",
-              Snippet = "",
-              Color = "󰏘",
-              File = "󰈚",
-              Reference = "󰈇",
-              Folder = "󰉋",
-              EnumMember = "",
-              Constant = "󰏿",
-              Struct = "󰙅",
-              Event = "",
-              Operator = "󰆕",
-              TypeParameter = "󰊄",
-              Table = "",
-              Object = "󰅩",
-              Tag = "",
-              Array = "[]",
-              Boolean = "",
-              Number = "",
-              Null = "󰟢",
-              String = "󰉿",
-              Calendar = "",
-              Watch = "󰥔",
-              Package = "",
-              Copilot = "",
-              Codeium = "",
-              TabNine = "",
-            }
+            function(entry, item)
+              local icons = {
+                Namespace = "󰌗",
+                Text = "󰉿",
+                Method = "󰆧",
+                Function = "󰆧",
+                Constructor = "",
+                Field = "󰜢",
+                Variable = "󰀫",
+                Class = "󰠱",
+                Interface = "",
+                Module = "",
+                Property = "󰜢",
+                Unit = "󰑭",
+                Value = "󰎠",
+                Enum = "",
+                Keyword = "󰌋",
+                Snippet = "",
+                Color = "󰏘",
+                File = "󰈚",
+                Reference = "󰈇",
+                Folder = "󰉋",
+                EnumMember = "",
+                Constant = "󰏿",
+                Struct = "󰙅",
+                Event = "",
+                Operator = "󰆕",
+                TypeParameter = "󰊄",
+                Table = "",
+                Object = "󰅩",
+                Tag = "",
+                Array = "[]",
+                Boolean = "",
+                Number = "",
+                Null = "󰟢",
+                String = "󰉿",
+                Calendar = "",
+                Watch = "󰥔",
+                Package = "",
+                Copilot = "",
+                Codeium = "",
+                TabNine = "",
+              }
 
-            local icon = icons[item.kind] or ""
+              local icon = icons[item.kind] or ""
               item.kind = string.format("%s %s", icon, item.kind or "")
+
+              local menu_icon = {
+                nvim_lsp = "NLSP",
+                nvim_lua = "NLUA",
+                luasnip  = "LSNP",
+                buffer   = "BUFF",
+                path     = "PATH",
+              }
+
+              item.menu = menu_icon[entry.source.name]
+
+              -- fixed_width = 60
+              fixed_width = fixed_width or false
+
+              local content = item.abbr
+
+              if fixed_width then
+                vim.o.pumwidth = fixed_width
+              end
+
+              local win_width = vim.api.nvim_win_get_width(0)
+              local max_content_width =
+                fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+              if #content > max_content_width then
+                item.abbr =
+                  vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+              else
+                item.abbr =
+                  content .. string.rep(" ", max_content_width - #content)
+              end
+
               return item
             end
           '';
