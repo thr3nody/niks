37679d3b1e6a5dd042c12b0532fa3c4bdd00999f
Erine Moira <me@erine.dev>
2026-02-05 02:13:40 +0700

    Zen: Remove ExtensionSettings, doesn't seem to work.

--- modules/home/programs/zen-browser/default.nix
+++ modules/home/programs/zen-browser/default.nix
@@ -37,35 +37,6 @@
         Remove = ["Google" "Bing" "Perplexity"];
       };
       SearchSuggestEnabled = true;
-      ExtensionSettings = {
-        "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi" = {
-          installation_mode = "force_installed";
-        };
-        "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi" = {
-          installation_mode = "force_installed";
-        };
-        "https://addons.mozilla.org/firefox/downloads/file/3403419/export_cookies_txt-0.3.2.xpi" = {
-          installation_mode = "force_installed";
-        };
-        "https://addons.mozilla.org/firefox/downloads/latest/noscript/latest.xpi" = {
-          installation_mode = "force_installed";
-        };
-        "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi" = {
-          installation_mode = "force_installed";
-        };
-        "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi" = {
-          installation_mode = "force_installed";
-        };
-        "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi" = {
-          installation_mode = "force_installed";
-        };
-        "https://addons.mozilla.org/firefox/downloads/latest/vue-js-devtools/latest.xpi" = {
-          installation_mode = "force_installed";
-        };
-        "https://addons.mozilla.org/firefox/downloads/latest/wappalyzer/latest.xpi" = {
-          installation_mode = "force_installed";
-        };
-      };
     };
     profiles."default" = {
       id = 0;
