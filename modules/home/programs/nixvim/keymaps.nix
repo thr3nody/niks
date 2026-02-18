af4e4899424920ece443c01c9a958c969e984671
Erine Moira <me@erine.dev>
2026-02-18 15:15:48 +0700

    Nixvim(utils): Remove some unused plugins.

--- modules/home/programs/nixvim/keymaps.nix
+++ modules/home/programs/nixvim/keymaps.nix
@@ -11,11 +11,6 @@
         action = "<cmd>Oil<cr>";
         options.desc = "Oil";
       }
-      # {
-      #   key = "<leader>e";
-      #   action = "<cmd>Neotree toggle right<cr>";
-      #   options.desc = "Neotree";
-      # }
       {
         key = "<leader>e";
         action = "<cmd>lua Snacks.explorer()<cr>";
@@ -49,62 +44,6 @@
         action = "<cmd>lua vim.lsp.buf.hover()<cr>";
         options.desc = "LSP Hover";
       }
-
-      # {
-      #   key = "<leader> ";
-      #   action = "<cmd>lua require('telescope.builtin').find_files()<cr>";
-      #   options.desc = "Telescope";
-      # }
-      # {
-      #   key = "<leader>sg";
-      #   action = "<cmd>lua require('telescope.builtin').live_grep()<cr>";
-      #   options.desc = "Livegrep";
-      # }
-      # {
-      #   key = "<leader>sf";
-      #   action = "<cmd>lua require('telescope.builtin').find_files()<cr>";
-      #   options.desc = "Find Files";
-      # }
-      # {
-      #   key = "<leader>sb";
-      #   action = "<cmd>lua require('telescope.builtin').buffers()<cr>";
-      #   options.desc = "Buffers";
-      # }
-      # {
-      #   key = "<leader>sh";
-      #   action = "<cmd>lua require('telescope.builtin').help_tags()<cr>";
-      #   options.desc = "Help Tags";
-      # }
-      # {
-      #   key = "<leader>sc";
-      #   action = "<cmd>lua require('telescope.builtin').commands()<cr>";
-      #   options.desc = "Commands";
-      # }
-      # {
-      #   key = "<leader>sm";
-      #   action = "<cmd>lua require('telescope.builtin').marks()<cr>";
-      #   options.desc = "Marks";
-      # }
-      # {
-      #   key = "<leader>so";
-      #   action = "<cmd>lua require('telescope.builtin').oldfiles()<cr>";
-      #   options.desc = "Oldfiles";
-      # }
-      # {
-      #   key = "<leader>ss";
-      #   action = "<cmd>lua require('telescope.builtin').spell_suggest()<cr>";
-      #   options.desc = "Spell Suggest";
-      # }
-      # {
-      #   key = "<leader>sr";
-      #   action = "<cmd>:noh<return><esc>";
-      #   options.desc = "Reset search";
-      # }
-      # {
-      #   key = "<leader>st";
-      #   action = "<cmd>TodoTelescope<cr>";
-      #   options.desc = "Todos";
-      # }
 
       {
         key = "<leader> ";
--- modules/home/programs/nixvim/plugins/utils.nix
+++ modules/home/programs/nixvim/plugins/utils.nix
@@ -1,12 +1,7 @@
 {
   programs.nixvim.plugins = {
     muren.enable = true;
-    treesj = {
-      enable = true;
-      settings.use_default_keymaps = true;
-    };
     flash.enable = true;
-    # image.enable = true;
     tmux-navigator.enable = true;
     comment.enable = true;
     nvim-autopairs.enable = true;
@@ -37,37 +32,5 @@
         highlight.enable = true;
       };
     };
-    wrapping = {
-      enable = true;
-      settings = {
-        auto_set_mode_filetype_allowlist = [
-          "markdown"
-          "text"
-          "latex"
-          "typst"
-        ];
-        create_commands = true;
-        create_keymaps = true;
-        notify_on_switch = true;
-        set_nvim_opt_default = true;
-      };
-    };
-    # harpoon = {
-    #   enable = true;
-    #   enableTelescope = true;
-    #   keymapsSilent = true;
-    #   keymaps = {
-    #     addFile = "<leader>ha";
-    #     toggleQuickMenu = "<leader>hu";
-    #     navNext = "<leader>hl";
-    #     navPrev = "<leader>hh";
-    #     navFile = {
-    #       "1" = "<leader>h1";
-    #       "2" = "<leader>h2";
-    #       "3" = "<leader>h3";
-    #       "4" = "<leader>h4";
-    #     };
-    #   };
-    # };
   };
 }
