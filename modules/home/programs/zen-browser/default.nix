e4ffc8cf22f2e60cd7e540d01b7247bdf2dd9a83
Erine Moira <me@erine.dev>
2026-02-23 16:14:43 +0700

    zen-browser: Suppress XDG migration warning.

--- modules/home/programs/zen-browser/default.nix
+++ modules/home/programs/zen-browser/default.nix
@@ -5,6 +5,7 @@
 
   programs.zen-browser = {
     enable = true;
+    suppressXdgMigrationWarning = true;
     policies = {
       AutofillAddressEnabled = true;
       AutofillCreditCardEnabled = false;
