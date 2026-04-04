53460a09a432a4d490241f9a4671d453a066367b
Erine Moira <me@erine.dev>
2026-04-04 19:54:42 +0700

    zen-browser: Remove suppressXdgMigrationWarning option.

--- modules/home/programs/zen-browser/default.nix
+++ modules/home/programs/zen-browser/default.nix
@@ -5,7 +5,6 @@
 
   programs.zen-browser = {
     enable = true;
-    suppressXdgMigrationWarning = true;
     policies = {
       AutofillAddressEnabled = true;
       AutofillCreditCardEnabled = false;
