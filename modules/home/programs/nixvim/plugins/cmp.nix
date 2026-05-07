1c9d60a35e1e6c76e643ad809b10825ebbcc97c0
Erine Moira <me@erine.dev>
2026-05-07 10:11:19 +0700

    nixvim(cmp): Remove cmp-emoji.

--- modules/home/programs/nixvim/plugins/cmp.nix
+++ modules/home/programs/nixvim/plugins/cmp.nix
@@ -2,7 +2,6 @@
   programs.nixvim.plugins = {
     luasnip.enable = true;
     cmp-buffer.enable = true;
-    cmp-emoji.enable = true;
     cmp-nvim-lsp.enable = true;
     cmp-path.enable = true;
     cmp-dap.enable = true;
