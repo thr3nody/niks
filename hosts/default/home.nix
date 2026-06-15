c9cb69a30ce57d7bb71363d2ddb56e511e108fed
Erine Moira <me@erine.dev>
2026-06-15 17:23:49 +0700

    hypr(land): Modularity thingy as well.

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -52,28 +52,16 @@
     homeDirectory = "/home/erine";
   };
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  wm.hyprland = {
+    monitors = ["eDP-1,1920x1080@144,0x0,1" "HDMI-A-1,1920x1080@60,auto,1,mirror,eDP-1"];
+    mouseSens = -0.6;
+    touchpadScrollFactor = 0.4;
+    devices = [
+      {
+        name = "elan1203:00-04f3:307a-touchpad";
+        sensitivity = 0.5;
+      }
+    ];
   };
 
   # Gnome GTK4 set all to dark theme.
@@ -82,26 +70,19 @@
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/erine/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 }
--- modules/home/wm/hypr/land.nix
+++ modules/home/wm/hypr/land.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   config,
   ...
 }: {
@@ -18,213 +19,246 @@
     ./lock.nix
   ];
 
-  home.packages = with pkgs; [
-    grimblast
-    libnotify
-  ];
+  options.wm.hyprland = {
+    monitors = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+    };
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.enable = false;
-    settings = with config.colorScheme.palette; {
-      "$mod" = "SUPER";
-      "$supermod" = "SUPERSHIFT";
+    devices = lib.mkOption {
+      type = lib.types.listOf (lib.types.attrsOf lib.types.anything);
+      default = [];
+      description = "Hyprland per-device config block.";
+    };
 
-      "$menu" = "tofi-run | xargs hyprctl dispatch exec --";
-      "$bar" = "pkill waybar || waybar &";
-      "$clipboard" = "kitty --class clipse -e clipse";
-      "$color-picker-hex" = "hyprpicker -a -f hex";
-      "$color-picker-rgb" = "hyprpicker -a -f rgb";
-      "$clock" = "pkill peaclock || kitty --class peaclock -e peaclock";
-      "$screenshot" = "grimblast --notify copysave area \"./Pictures/Screenshots/$(date +'%Y-%m-%d %H:%M:%S').png\"";
-      "$screenshot-full" = "grimblast --notify copysave screen \"./Pictures/Screenshots/$(date +'%Y-%m-%d %H:%M:%S').png\"";
+    mouseSens = lib.mkOption {
+      type = lib.types.float;
+      default = -0.6;
+      description = "Global mouse sensitivity.";
+    };
 
-      bind =
-        [
-          "$supermod, B, exec, nvidia-offload zen"
-          "$supermod, RETURN, exec, kitty --class secretty"
-          "$supermod, P, exec, $color-picker-rgb"
+    touchpadScrollFactor = lib.mkOption {
+      type = lib.types.float;
+      default = 0.4;
+      description = "Global touchpad scroll factor.";
+    };
 
-          "$mod, B, exec, zen-beta"
-          "$mod, D, exec, signal-desktop"
-          "$mod, T, exec, thunderbird"
-          "$mod, RETURN, exec, kitty"
-          "$mod, SPACE, exec, $menu"
-          "$mod, M, exec, kitty --class moosic -e musikcube"
-          "$mod, P, exec, $color-picker-hex"
-          "$mod, W, exec, $bar"
-          "$mod, E, exec, Thunar"
-          "$mod, V, exec, $clipboard"
-          "$mod, C, exec, $clock"
+    extraBinds = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "Extra keybind entries.";
+    };
 
-          "$mod, F, fullscreen"
-          "$mod, Q, killactive"
-          "$mod, A, togglefloating"
+    extraExecOnce = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "Extra exec-once entries.";
+    };
+  };
 
-          "$mod, H, movefocus, l"
-          "$mod, L, movefocus, r"
-          "$mod, K, movefocus, u"
-          "$mod, J, movefocus, d"
+  config = {
+    home.packages = with pkgs; [
+      grimblast
+      libnotify
+    ];
 
-          "$supermod, H, movewindow, l"
-          "$supermod, L, movewindow, r"
-          "$supermod, K, movewindow, u"
-          "$supermod, J, movewindow, d"
+    wayland.windowManager.hyprland = {
+      enable = true;
+      systemd.enable = false;
+      settings = with config.colorScheme.palette; {
+        "$mod" = "SUPER";
+        "$supermod" = "SUPERSHIFT";
 
-          "$mod, mouse_up, workspace, e+1"
-          "$mod, mouse_down, workspace, e-1"
+        "$menu" = "tofi-run | xargs hyprctl dispatch exec --";
+        "$bar" = "pkill waybar || waybar &";
+        "$clipboard" = "kitty --class clipse -e clipse";
+        "$color-picker-hex" = "hyprpicker -a -f hex";
+        "$color-picker-rgb" = "hyprpicker -a -f rgb";
+        "$clock" = "pkill peaclock || kitty --class peaclock -e peaclock";
+        "$screenshot" = "grimblast --notify copysave area \"./Pictures/Screenshots/$(date +'%Y-%m-%d %H:%M:%S').png\"";
+        "$screenshot-full" = "grimblast --notify copysave screen \"./Pictures/Screenshots/$(date +'%Y-%m-%d %H:%M:%S').png\"";
 
-          "$supermod, Q, exec, uwsm stop"
-          "$supermod, BACKSPACE, exec, hyprlock"
+        bind =
+          [
+            "$supermod, RETURN, exec, kitty --class secretty"
+            "$supermod, P, exec, $color-picker-rgb"
 
-          "$mod, 0, workspace, 10"
-          "$supermod, 0, movetoworkspace, 10"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (
-              i: let
-                ws = i + 1;
-              in [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-              ]
-            )
-            9)
-        );
+            "$mod, B, exec, zen-beta"
+            "$mod, D, exec, signal-desktop"
+            "$mod, T, exec, thunderbird"
+            "$mod, RETURN, exec, kitty"
+            "$mod, SPACE, exec, $menu"
+            "$mod, M, exec, kitty --class moosic -e musikcube"
+            "$mod, P, exec, $color-picker-hex"
+            "$mod, W, exec, $bar"
+            "$mod, E, exec, Thunar"
+            "$mod, V, exec, $clipboard"
+            "$mod, C, exec, $clock"
 
-      bindl = [
-        "$mod, S, exec, $screenshot"
-        "$supermod, S, exec, $screenshot-full"
-        ", PRINT, exec, $screenshot-full"
-      ];
+            "$mod, F, fullscreen"
+            "$mod, Q, killactive"
+            "$mod, A, togglefloating"
 
-      binde = [
-        ", XF86AudioRaiseVolume, exec, swayosd-client --output-volume raise"
-        ", XF86AudioLowerVolume, exec, swayosd-client --output-volume lower"
+            "$mod, H, movefocus, l"
+            "$mod, L, movefocus, r"
+            "$mod, K, movefocus, u"
+            "$mod, J, movefocus, d"
 
-        ", XF86MonBrightnessUp, exec, swayosd-client --brightness raise"
-        ", XF86MonBrightnessDown, exec, swayosd-client --brightness lower"
+            "$supermod, H, movewindow, l"
+            "$supermod, L, movewindow, r"
+            "$supermod, K, movewindow, u"
+            "$supermod, J, movewindow, d"
 
-        ", Caps_Lock, exec, sleep 0.07; swayosd-client --caps-lock"
-        ", Num_Lock, exec, sleep 0.07; swayosd-client --num-lock"
-      ];
+            "$mod, mouse_up, workspace, e+1"
+            "$mod, mouse_down, workspace, e-1"
 
-      bindm = [
-        "$mod, mouse:272, movewindow"
-        "$mod, mouse:273, resizewindow"
-      ];
+            "$supermod, Q, exec, uwsm stop"
+            "$supermod, BACKSPACE, exec, hyprlock"
 
-      monitor = [
-        "eDP-1,1920x1080@144,0x0,1"
-        "HDMI-A-1,1920x1080@60,auto,1,mirror,eDP-1"
-      ];
+            "$mod, 0, workspace, 10"
+            "$supermod, 0, movetoworkspace, 10"
+          ]
+          ++ config.wm.hyprland.extraBinds
+          ++ (
+            # workspaces
+            # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+            builtins.concatLists (builtins.genList (
+                i: let
+                  ws = i + 1;
+                in [
+                  "$mod, code:1${toString i}, workspace, ${toString ws}"
+                  "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+                ]
+              )
+              9)
+          );
 
-      general = {
-        gaps_in = 6;
-        gaps_out = 10;
-        border_size = 2;
-        "col.active_border" = "rgba(${base09}ee)";
-        "col.inactive_border" = "rgba(${base02}ee)";
-      };
-
-      input = {
-        kb_options = "compose:ralt";
-
-        touchpad = {
-          natural_scroll = true;
-          scroll_factor = 0.4;
-        };
-
-        sensitivity = -0.6;
-        accel_profile = "flat";
-      };
-
-      gesture = ["3, vertical, workspace"];
-
-      device = {
-        name = "elan1203:00-04f3:307a-touchpad";
-        sensitivity = 0.5;
-      };
-
-      decoration = {
-        shadow = {
-          enabled = true;
-          render_power = 3;
-          color = "rgba(${base02}ee)";
-          color_inactive = "rgba(${base00}ee)";
-          range = 50;
-        };
-        rounding = 15;
-        blur = {
-          enabled = true;
-          size = 8;
-          passes = 4;
-          new_optimizations = true;
-          ignore_opacity = true;
-        };
-      };
-
-      dwindle = {
-        pseudotile = 1; # Enable pseudotiling on dwindle
-        force_split = 0;
-      };
-
-      animations = {
-        enabled = 1;
-        bezier = ["overshot,0.13,0.99,0.29,1.1"];
-        animation = [
-          "windows,1,4,overshot,slide"
-          "border,1,10,default"
-          "fade,1,10,default"
-          "workspaces,1,6,overshot,slidevert"
+        bindl = [
+          "$mod, S, exec, $screenshot"
+          "$supermod, S, exec, $screenshot-full"
+          ", PRINT, exec, $screenshot-full"
         ];
+
+        binde = [
+          ", XF86AudioRaiseVolume, exec, swayosd-client --output-volume raise"
+          ", XF86AudioLowerVolume, exec, swayosd-client --output-volume lower"
+
+          ", XF86MonBrightnessUp, exec, swayosd-client --brightness raise"
+          ", XF86MonBrightnessDown, exec, swayosd-client --brightness lower"
+
+          ", Caps_Lock, exec, sleep 0.07; swayosd-client --caps-lock"
+          ", Num_Lock, exec, sleep 0.07; swayosd-client --num-lock"
+        ];
+
+        bindm = [
+          "$mod, mouse:272, movewindow"
+          "$mod, mouse:273, resizewindow"
+        ];
+
+        monitor = config.wm.hyprland.monitors;
+
+        general = {
+          gaps_in = 6;
+          gaps_out = 10;
+          border_size = 2;
+          "col.active_border" = "rgba(${base09}ee)";
+          "col.inactive_border" = "rgba(${base02}ee)";
+        };
+
+        input = {
+          kb_options = "compose:ralt";
+
+          touchpad = {
+            natural_scroll = true;
+            scroll_factor = 0.4;
+          };
+
+          sensitivity = -0.6;
+          accel_profile = "flat";
+        };
+
+        gesture = ["3, vertical, workspace"];
+
+        device = config.wm.hyprland.devices;
+
+        decoration = {
+          shadow = {
+            enabled = true;
+            render_power = 3;
+            color = "rgba(${base02}ee)";
+            color_inactive = "rgba(${base00}ee)";
+            range = 50;
+          };
+          rounding = 15;
+          blur = {
+            enabled = true;
+            size = 8;
+            passes = 4;
+            new_optimizations = true;
+            ignore_opacity = true;
+          };
+        };
+
+        dwindle = {
+          pseudotile = 1; # Enable pseudotiling on dwindle
+          force_split = 0;
+        };
+
+        animations = {
+          enabled = 1;
+          bezier = ["overshot,0.13,0.99,0.29,1.1"];
+          animation = [
+            "windows,1,4,overshot,slide"
+            "border,1,10,default"
+            "fade,1,10,default"
+            "workspaces,1,6,overshot,slidevert"
+          ];
+        };
+
+        windowrule = [
+          "workspace 5, match:class moosic"
+          "workspace 4, match:class Code"
+          "workspace 4, match:class dev.zed.Zed"
+          "workspace 4, match:class Godot_Engine"
+          "workspace 3, match:class firefox"
+          "workspace 3, match:class zen"
+          "workspace 3, match:class zen-beta"
+          "workspace 3, match:class zen-twilight"
+          "workspace 1, match:class legcord"
+          "workspace 1, match:class discord"
+          "workspace 1, match:class vesktop"
+          "workspace 1, match:class signal"
+
+          "float on, match:class org.pulseaudio.pavucontrol"
+          "size 622 652, match:class org.pulseaudio.pavucontrol"
+          "float on, match:class clipse"
+          "size 622 652, match:class clipse"
+          "float on, match:class peaclock"
+          "size 360 230, match:class peaclock"
+
+          "no_screen_share on, match:class signal"
+          "no_screen_share on, match:class vesktop"
+          "no_screen_share on, match:class discord"
+          "no_screen_share on, match:class legcord"
+          "no_screen_share on, match:class Thunar"
+          "no_screen_share on, match:class org.gnome.seahorse.Application"
+          "no_screen_share on, match:class thunderbird"
+          "no_screen_share on, match:class secretty"
+          "no_screen_share on, match:class clipse"
+
+          "no_screen_share on, match:title .*[Ww]hats[Aa]pp.*"
+          "no_screen_share on, match:title .*[Bb]itwarden.*"
+          "no_screen_share on, match:title .*[Gg]mail.*"
+        ];
+
+        layerrule = [
+          "no_screen_share on, match:namespace notifications"
+        ];
+
+        cursor.no_hardware_cursors = false;
+
+        exec-once = ["clipse -listen"] ++ config.wm.hyprland.extraExecOnce;
       };
-
-      windowrule = [
-        "workspace 5, match:class moosic"
-        "workspace 4, match:class Code"
-        "workspace 4, match:class dev.zed.Zed"
-        "workspace 4, match:class Godot_Engine"
-        "workspace 3, match:class firefox"
-        "workspace 3, match:class zen"
-        "workspace 3, match:class zen-beta"
-        "workspace 3, match:class zen-twilight"
-        "workspace 1, match:class legcord"
-        "workspace 1, match:class discord"
-        "workspace 1, match:class vesktop"
-        "workspace 1, match:class signal"
-
-        "float on, match:class org.pulseaudio.pavucontrol"
-        "size 622 652, match:class org.pulseaudio.pavucontrol"
-        "float on, match:class clipse"
-        "size 622 652, match:class clipse"
-        "float on, match:class peaclock"
-        "size 360 230, match:class peaclock"
-
-        "no_screen_share on, match:class signal"
-        "no_screen_share on, match:class vesktop"
-        "no_screen_share on, match:class discord"
-        "no_screen_share on, match:class legcord"
-        "no_screen_share on, match:class Thunar"
-        "no_screen_share on, match:class org.gnome.seahorse.Application"
-        "no_screen_share on, match:class thunderbird"
-        "no_screen_share on, match:class secretty"
-        "no_screen_share on, match:class clipse"
-
-        "no_screen_share on, match:title .*[Ww]hats[Aa]pp.*"
-        "no_screen_share on, match:title .*[Bb]itwarden.*"
-        "no_screen_share on, match:title .*[Gg]mail.*"
-      ];
-
-      layerrule = [
-        "no_screen_share on, match:namespace notifications"
-      ];
-
-      cursor.no_hardware_cursors = false;
-
-      exec-once = ["clipse -listen"];
     };
   };
 }
