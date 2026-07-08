5c9c11198bbbdec180e71d5d1f1f5226ae14fa37
Erine Moira <me@erine.dev>
2026-07-09 01:22:08 +0700

    Add: upower for quickshell thingy.

--- hosts/europa/configuration.nix
+++ hosts/europa/configuration.nix
@@ -194,6 +194,8 @@
     MemoryMax = "4096M";
   };
 
+  services.upower.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
